4e3e440261a5eeb7fb8bcb1bd91b6af06ed8722a
chagcode <chag.open@gmail.com>
2025-02-02 16:54:47 +0300

    Условные операторы

---
+++ src/CondOper.java
@@ -0,0 +1,10 @@
+public class CondOper {
+  public static void main(String[] args) {
+    int temp = 26;
+    if (temp > 25) {
+      System.out.println("Кондиционер включен");
+    } else {
+      System.out.println("Кондиционер выключен");
+    }
+  }
+}
