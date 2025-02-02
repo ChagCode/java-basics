4752548f876ae884b1a529fbb66629fc9b8ad90d
chagcode <chag.open@gmail.com>
2025-02-02 16:47:31 +0300

    variable

---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+  public static void main(String[] args) {
+    System.out.println("------------------------------- Задача 1 ----------------------------");
+    int john = 100; // У Джона 100$
+    int nick = 5 * 100; // у Ника в 5 раз больше
+    // сколько у них всего денег
+    System.out.println("Денег у Джона и Ника -> " + (john + nick));
+
+    System.out.println("------------------------------- Задача 2 ----------------------------");
+    // сколько в числе лет, месяцев и дней
+    int numb = 10000;
+    int years = numb / 365;
+    int months = (numb - (years * 365)) / 30;
+    int days = numb - (years *365) - (months * 30);
+
+    System.out.println("В числе " + numb + " лет -> " + years);
+    System.out.println("В числе " + numb + " месяцев-> " + months);
+    System.out.println("В числе " + numb + " дней -> " + days);
+  }
+}
